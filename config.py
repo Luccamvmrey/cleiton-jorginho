5f2817b82967cb1c8b24a95c2db57469425df417
Lucca Rey <104277321+Luccamvmrey@users.noreply.github.com>
2024-06-17 18:16:18 -0300

    First commit

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import speech_recognition as sr
 
 # Configure OpenAI API
-API_KEY = "sk-proj-QTqiu0QpMRCe9JEE6yGYT3BlbkFJMID4qCRtBW3mDQpAblRa"
+API_KEY = "Your OpenAI API key here"
 client = OpenAI(api_key=API_KEY)
 
 # Configure speech recognition
